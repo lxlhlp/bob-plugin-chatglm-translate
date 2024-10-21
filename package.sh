37ea94fd57f0c6101e188532de7b5d88b6662e14
李晓亮 <841480177@qq.com>
2024-10-21 10:07:40 +0800

    修复更新时间问题

--- package.sh
+++ package.sh
@@ -1,25 +1,29 @@
-zip -0 -j  -r ./bob-plugin-chatglm-translate.bobplugin ./src/*
+version=$(jq -r .version ./src/info.json)
+
+zip -0 -j  -r ./bob-plugin-chatglm-translate-v$version.bobplugin ./src/*
 
 
 version=$(jq -r .version ./src/info.json)
 
 echo $version
 
-hash=$(shasum -a 256 ./bob-plugin-chatglm-translate.bobplugin | cut -d ' ' -f 1)
+hash=$(shasum -a 256 ./bob-plugin-chatglm-translate-v$version.bobplugin | cut -d ' ' -f 1)
 
-timestamp=$(date +%s)
+timestamp=$(date +%s000)
 
 newVersion=$(cat << EOF
 {
   "version": "$version",
   "desc": "新增功能",
   "sha256": "$hash",
-  "url": "https://github.com/lxlhlp/bob-plugin-chatglm-translate/releases/download/$version/bob-plugin-chatglm-translate.bobplugin",
+  "url": "https://github.com/lxlhlp/bob-plugin-chatglm-translate/releases/download/v$version/bob-plugin-chatglm-translate-v$version.bobplugin",
   "minBobVersion": "1.8.0",
  "timestamp": $timestamp
 }
 EOF
 )
+
+echo $newVersion
 
 jq --argjson newVersion "$newVersion" '.versions |= [$newVersion] + .' ./appcast.json > temp.json
 
